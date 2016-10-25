012dc72501950a41ded9049b0cb41671d5cfc51a
Marek Dominiak <marek.dominiak@mintra.no>
2016-10-25 14:43:38 +0200

    improved pitest test

--- src/test/java/com/dominiak/wjugtests/pitest/CalculatorTest.java
+++ src/test/java/com/dominiak/wjugtests/pitest/CalculatorTest.java
@@ -3,21 +3,15 @@ package com.dominiak.wjugtests.pitest;
 import org.assertj.core.api.Assertions;
 import org.testng.annotations.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.testng.Assert.*;
 
 
 public class CalculatorTest {
     @Test
-    public void trickyTestWithNoAssertions() throws Exception {
+    public void shouldAddNumbersProperly() throws Exception {
         Calculator calculator = new Calculator();
 
-        Assertions.assertThat(calculator.sum(2, 2));
-    }
-
-    @Test
-    public void betterTestWithAssertion() throws Exception {
-        Calculator calculator = new Calculator();
-
-        Assertions.assertThat(calculator.sum(2, 2)).isEqualTo(4);
+        Assertions.assertThat(calculator.sum(2, 3));
     }
 }
